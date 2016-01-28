4a43a6a8e03b9ea4c68eafe5eca5cd808126de7d
Jakub Sokołowski <jakub.sokolowski@partners.mbank.pl>
2016-01-28 23:30:07 +0100

    Initial commit with project setup

---
+++ Elements/Elements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elements
+//
+//  Created by Jakub Sokołowski on 1/28/16.
+//  Copyright © 2016 jakub.sokolowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
