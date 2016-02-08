8dcbd92a467a3e78e2789350dee8fb7c0f7fcf85
Jakub Sokołowski <jakub.sokolowski@partners.mbank.pl>
2016-02-09 00:02:04 +0100

    Initial implementation of UI and Game VC

--- Elements/Elements/ViewControllers/GameViewController.swift
+++ Elements/Elements/ViewControllers/GameViewController.swift
@@ -11,10 +11,35 @@ import UIKit
 class GameViewController: UIViewController {
 
     private let viewModel = GameViewModel()
+    @IBOutlet weak var queryLabel: UILabel!
+    @IBOutlet var answersButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        queryLabel.text = viewModel.queryTitle
+        for (index,button) in answersButtons.enumerate() {
+            button.setTitle(viewModel.answers[index], forState: UIControlState.Normal)
+            button.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    
+    @IBAction func chosenAnswerPressed(sender: UIButton) {
+        let answer = sender.currentTitle!
+        sender.backgroundColor = viewModel.isAnswerCorrect(answer) ? UIColor.greenColor() : UIColor.redColor()
+        
+        let seconds = 1.0
+        let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        
+        dispatch_after(dispatchTime, dispatch_get_main_queue(), { [unowned self] in
+            self.viewModel.drawNext()
+            self.setupSubviews()
+        })
     }
 }
 
