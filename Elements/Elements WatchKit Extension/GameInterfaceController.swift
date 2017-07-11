36365b1700c257e6504ed6a8a85d30dd25984dcc
Kuba Sokolowski <jakub.m.sokolowski@gmail.com>
2017-07-11 13:48:48 +0200

    Add IBOutlets and IBActions to the GameInterfaceController

--- Elements/Elements WatchKit Extension/GameInterfaceController.swift
+++ Elements/Elements WatchKit Extension/GameInterfaceController.swift
@@ -11,21 +11,33 @@ import Foundation
 
 
 class GameInterfaceController: WKInterfaceController {
+    @IBOutlet var questionLabel: WKInterfaceLabel!
 
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
+    //MARK - Buttons IBOutlets
+    @IBOutlet var answerButton1: WKInterfaceButton!
+    @IBOutlet var answerButton2: WKInterfaceButton!
+    @IBOutlet var answerButton3: WKInterfaceButton!
+    @IBOutlet var answerButton4: WKInterfaceButton!
+    
+    //WatchKit does not support 'UI collections" so we have to create it on our own
+    private var buttons: [WKInterfaceButton] {
+        return [answerButton1, answerButton2, answerButton3, answerButton4]
+    }
+    
+    //MARK - User did select the answer - buttons handlers
+    //It's not possible to send any argument in the WKInterfaceButton's IBAction, so every single button has its own action
+    @IBAction func didSelectAnswer1() {
         
-        // Configure interface objects here.
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    
+    @IBAction func didSelectAnswer2() {
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    
+    @IBAction func didSelectAnswer3() {
     }
-
+    
+    @IBAction func didSelectAnswer4() {
+    }
+    
+    
 }
